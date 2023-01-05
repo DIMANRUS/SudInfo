b932d90726db7dfd0b8095c27bbcb59a8fbbcb06
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2023-01-05 11:02:57 +0300

    Remove unused projects

--- SudInfo.WPF/Stores/ActionStore.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SudInfo.WPF.Stores;
-public static class ActionStore
-{
-    public static Action CloseWindow { get; set; }
-}
--- SudInfo.WPF/Stores/WindowDialogStore.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SudInfo.WPF.Stores;
-public static class WindowDialogStore
-{
-    public static Action OpenAddComputerWindowDialog { get; set; }
-}
